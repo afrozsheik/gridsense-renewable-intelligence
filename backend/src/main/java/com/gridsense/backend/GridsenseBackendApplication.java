7f64540a5235b24c7bf049befe9d7183f820b77c
afrozsheik <166361913+afrozsheik@users.noreply.github.com>
2025-12-23 11:37:16 +0530

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/gridsense/backend/GridsenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gridsense.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridsenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridsenseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gridsense/backend/GridsenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridsense.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridsenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
